f24a1a59ee028f1d84331d3a30f279da3a761b03
adenosyl <rustem.kushtayev@gmail.com>
2025-11-03 20:27:36 +0500

    Фикс по ошибке build-filmorate#3

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,14 @@
+DELETE FROM film_likes;
+DELETE FROM film_genres;
+DELETE FROM friends;
+DELETE FROM films;
+DELETE FROM users;
+DELETE FROM genres;
+DELETE FROM mpa;
+
+ALTER TABLE users ALTER COLUMN id RESTART WITH 1;
+ALTER TABLE films ALTER COLUMN id RESTART WITH 1;
+
 MERGE INTO genres (id, name) KEY(id) VALUES
     (1, 'Комедия'),
     (2, 'Драма'),
